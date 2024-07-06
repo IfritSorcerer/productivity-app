e14205c86cd84b667aae9cd09003aaf69350471a
IfritSorcerer <ianisern@yahoo.com>
2024-07-06 12:14:54 -0400

    added styles

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,35 +1,37 @@
 import React from "react";
-
+import "../styles/taskform.css"
 export const TaskForm = ({
   title,
   setNewTitle,
   description,
   setNewDescription,
-  handleSubmit
+  handleSubmit,
 }) => {
- 
   return (
-    <form className="taskForm">
-      <label htmlFor="taskName">Title:</label>
-        <input
-          type="text"
-          htmlFor="taskName"
-          placeholder="Buy Eggs"
-          id="taskName"
-          value={title}
-          onChange={(e) => setNewTitle(e.target.value)}
-          required
-        />
-      <label htmlFor="taskDescription">Description:</label>
-        <input
-          htmlFor="taskDescripion"
-          id="taskDescription"
-          placeholder="Please don't forget to buy eggs later from the store"
-          value={description}
-          onChange={(e) => setNewDescription(e.target.value)}
-          required
-        />
-      <button type="submit" onClick={handleSubmit}>
+    
+    <form class="taskForm">
+      <h4>What needs to get done?</h4>
+      <label htmlFor="taskName" id="taskLabel">Task:</label>
+      <input
+        type="text"
+        htmlFor="taskName"
+        placeholder="Buy Eggs"
+        id="taskInput"
+        value={title}
+        onChange={(e) => setNewTitle(e.target.value)}
+        required
+      />
+      <br />
+      <label htmlFor="taskDescription" id="taskLabel">Description:</label>
+      <input
+        htmlFor="taskDescripion"
+        id="taskInput"
+        placeholder="Please don't forget to buy eggs later from the store"
+        value={description}
+        onChange={(e) => setNewDescription(e.target.value)}
+        required
+      />
+      <button type="submit" id="taskButton" onClick={handleSubmit}>
         Add Task
       </button>
     </form>
